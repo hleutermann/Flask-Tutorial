0e7f4072131a74f47793c7d9131873de5266182e
hleutermann <77309561+hleutermann@users.noreply.github.com>
2021-03-28 13:15:46 -0500

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -23,7 +24,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Hello, World!'
 
