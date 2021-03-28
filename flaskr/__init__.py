219b4ee3126d3a47f78299e8a923bfe06d7eda1b
hleutermann <77309561+hleutermann@users.noreply.github.com>
2021-03-28 13:32:00 -0500

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,8 +24,11 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
 
     return app
