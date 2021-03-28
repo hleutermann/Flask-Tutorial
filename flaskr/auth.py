5082196d041589b53b8404925ff44950c00c3ad5
hleutermann <77309561+hleutermann@users.noreply.github.com>
2021-03-28 14:32:42 -0500

    Update auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -67,11 +67,11 @@ def load_logged_in_user():
     user_id = session.get('user_id')
 
     if user_id is None:
-            g.user = None
-        else:
-            g.user = get_db().execute(
-                'SELECT * FROM user WHERE id = ?', (user_id,)
-            ).fetchone()
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
 @bp.route('/logout')
 def logout():
